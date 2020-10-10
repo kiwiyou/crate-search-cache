f078808f20ab40fdd140d0f965456a2be423588c
kiwiyou <kiwiyou.dev@gmail.com>
2020-10-11 00:26:58 +0900

    Add dependency information

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{http::StatusCode, web, App, HttpResponse, HttpServer, ResponseError};
 use sqlx::{postgres::PgConnectOptions, PgPool};
 
 #[actix_web::main]
@@ -32,9 +32,21 @@ struct CrateInfo {
 
 #[derive(sqlx::FromRow)]
 struct VersionInfo {
+    id: i32,
     num: String,
     crate_size: Option<i32>,
     license: Option<String>,
+}
+
+#[derive(sqlx::FromRow)]
+struct DependencyInfo {
+    kind: i32,
+}
+
+mod dependency_kind {
+    pub const NORMAL: i32 = 0;
+    pub const BUILD: i32 = 1;
+    pub const DEV: i32 = 2;
 }
 
 #[derive(serde::Serialize)]
@@ -53,6 +65,28 @@ struct FindCrateResponse {
     #[serde(skip_serializing_if = "Option::is_none")]
     homepage: Option<String>,
     updated_at: chrono::DateTime<chrono::Utc>,
+    dependencies: usize,
+    dev_dependencies: usize,
+    build_dependencies: usize,
+}
+
+#[derive(Debug)]
+struct SqlxError(sqlx::Error);
+
+impl std::fmt::Display for SqlxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl ResponseError for SqlxError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code()).body("Internal Server Error")
+    }
 }
 
 async fn find_crate(
@@ -73,10 +107,11 @@ async fn find_crate(
     .bind(path.as_str())
     .fetch_optional(pool.as_ref())
     .await
-    .map_err(|_| actix_web::error::ErrorInternalServerError("Internal Server Error"))?
+    .map_err(SqlxError)?
     .ok_or_else(|| actix_web::error::ErrorNotFound("Not Found"))?;
     let version_info: VersionInfo = sqlx::query_as(
-        "SELECT num, \
+        "SELECT id, \
+                num, \
                 crate_size, \
                 license \
         FROM versions \
@@ -87,7 +122,25 @@ async fn find_crate(
     .bind(crate_info.id)
     .fetch_one(pool.as_ref())
     .await
-    .map_err(|_| actix_web::error::ErrorInternalServerError("Internal Server Error"))?;
+    .map_err(SqlxError)?;
+    let dependencies: Vec<DependencyInfo> = sqlx::query_as(
+        "SELECT kind \
+        FROM dependencies \
+        WHERE version_id = $1",
+    )
+    .bind(version_info.id)
+    .fetch_all(pool.as_ref())
+    .await
+    .map_err(SqlxError)?;
+    let (dependencies, build_dependencies, dev_dependencies) =
+        dependencies
+            .iter()
+            .fold((0, 0, 0), |(n, b, d), dep| match dep.kind {
+                dependency_kind::NORMAL => (n + 1, b, d),
+                dependency_kind::BUILD => (n, b + 1, d),
+                dependency_kind::DEV => (n, b, d + 1),
+                _ => (n, b, d),
+            });
     let result = FindCrateResponse {
         version: version_info.num,
         description: crate_info.description,
@@ -97,6 +150,9 @@ async fn find_crate(
         documentation: crate_info.documentation,
         homepage: crate_info.homepage,
         updated_at: chrono::DateTime::from_utc(crate_info.updated_at, chrono::Utc),
+        dependencies,
+        build_dependencies,
+        dev_dependencies,
     };
     Ok(web::Json(result))
 }
